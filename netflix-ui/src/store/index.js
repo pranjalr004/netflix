0491d1b2d59e9e1e2f8d3f25a9fae32831358d96
pranjalr004 <pranjalr376@gmail.com>
2025-02-17 07:55:45 +0530

    Fetching Genres from TMDB API

--- netflix-ui/src/store/index.js
+++ netflix-ui/src/store/index.js
@@ -1,4 +1,6 @@
 import {configureStore,createAsyncThunk,createSlice} from "@reduxjs/toolkit"
+import { API_KEY, TMBD_BASE_URL } from "../utils/constants"
+import axios from "axios"
 
 const initialState={
     movies:[],
@@ -6,10 +8,23 @@ const initialState={
     genres:[],
 }
 
+export const getGenres=createAsyncThunk("netflix/genres",async()=>{
+   const {
+    data:{genres},
+   } =await axios.get(`${TMBD_BASE_URL}/genre/movie/list?api_key=${API_KEY}`)
+   return genres
+   
+})
+
 const NetflixSlice=createSlice({
     name:"Netflix",
     initialState,
-    extraReducers:(builder)=>{}
+    extraReducers:(builder)=>{
+        builder.addCase(getGenres.fulfilled,(state,action)=>{
+            state.genres=action.payload;
+            state.genresLoaded=true
+        })
+    }
 })
 
 
